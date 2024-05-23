33e6d7681dbadfb47f6a1ce0824dd3f99c042224
surajgajul <95496170+surajgajul@users.noreply.github.com>
2024-05-23 11:38:43 +0530

    changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import SalesListAPIView
 
 urlpatterns = [
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path('api/sales/', SalesListAPIView.as_view(), name='sales-list'),
 ]
